33f4842d966a3af6a4679bc65ec4ca1eea2beea6
Deepak Maharana <dmaharana@gmail.com>
2025-08-01 22:02:38 -0400

    fix(scripts): correct directory navigation in build script - Changed `cd $SERVER_DIR` to `cd $WEB_DIR && cd $SERVER_DIR` to ensure proper path resolution - Added sequential directory navigation for execution context consistency
    This addresses a potential issue where the script might fail if executed from an unexpected working directory, ensuring it first moves to `WEB_DIR` before accessing `SERVER_DIR`. The double `&&` operator ensures the second `cd` only executes if the first succeeds.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-WEB_DIR=`pwd`
+WEB_DIR=${PWD##*/}
 SERVER_DIR="server"
 WEB_PORT=5000
 SERVER_REPO="git@github.com:dmaharana/todo_app_react_py_server.git"
@@ -9,6 +9,6 @@ pnpm build && \
 cp -r dist ../${SERVER_DIR}
 
 # 2. Run server
-cd ${SERVER_DIR} && \
+cd ../${SERVER_DIR} && \
 pip install -r requirements.txt && \
 python api.py --port 5000 --build-dir dist
